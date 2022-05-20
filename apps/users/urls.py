e7273aad07715a5327ef83517d38b3fda0b50343
rabbit16 <1214662198@qq.com>
2022-05-20 08:50:00 +0800

    向新文件夹添加了前端内容，补充了路由

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -10,5 +10,10 @@ app_name = 'index'
 urlpatterns = [
     path('', views.index.as_view(), name='see_index'),
     path("login/", views.Login.as_view(), name="login"),
-    path("register/", views.Register.as_view(), name="register")
+    path("register/", views.Register.as_view(), name="register"),
+    path("index_test/", views.Index_test.as_view(), name="index_test"),
+    path("movie/", views.Movie.as_view(), name="movie"),
+    path("rank/", views.Rank.as_view(), name="rank"),
+    path("movieDetail/", views.MovieDetail.as_view(), name="movieDetail"),
+    path("ticket/", views.Ticket.as_view(), name="ticket"),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,7 +1,7 @@
 import json
 
 from django.contrib.auth import login
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 from django.views import View
@@ -10,6 +10,7 @@ from django.views import View
 from users.models import User
 from utils.res_code import to_json_data, Code, error_map
 from verifications.forms import RegisterForm
+from django.contrib.auth import authenticate, login
 
 
 class index(View):
@@ -20,15 +21,50 @@ class index(View):
     def post(self, request):
         pass
 
+class Index_test(View):
+
+    def get(self, request):
+        return render(request, "index/index_test.html")
+
+    def post(self, request):
+        pass
+
+
 class Login(View):
 
     def get(self, request):
         return render(request, "index/login.html")
 
+    # def post(self, request):
+    #     return json.dumps({
+    #         "errno": '1'
+    #     })
     def post(self, request):
-        return json.dumps({
-            "errno": '1'
-        })
+
+        user_info = json.loads(request.body.decode())
+        username = user_info.get("username")
+        password = user_info.get("password")
+        # 与数据库中的用户名和密码比对，django默认保存密码是以哈希形式存储，并不是明文密码，这里的password验证默认调用的是User类的check_password方法，以哈希值比较。
+        user = authenticate(request, username=username, password=password)
+        try:
+            # 验证如果用户不为空
+            if user is not None:
+                # login方法登录
+                login(request, user)
+                # data = {
+                #     'errno': Code.OK
+                # }
+            return to_json_data(errno=Code.OK, errmsg=error_map[Code.OK])
+        except:
+            return to_json_data(errno=Code.NODATA, errmsg=error_map[Code.PICERROR])
+        #     # 返回登录成功信息
+        #     # return HttpResponse('index.html')
+        #     return redirect("/index/") #这点确实不会，等学长
+        #     # return render(request, 'index.html')
+        # else:
+        #     # 返回登录失败信息
+        #     return HttpResponse('登陆失败，未注册或密码错误！')
+
 #
 class Register(View):
 
@@ -42,6 +78,7 @@ class Register(View):
         else:
             userInfo["gender"] = False
         registerForm = RegisterForm(userInfo)
+
         try:
             if registerForm.is_valid():
                 user = User.objects.create_user(username=registerForm.cleaned_data.get('username'),
@@ -59,3 +96,47 @@ class Register(View):
             return to_json_data(data=data)
         except:
             return to_json_data(errno=Code.NODATA, errmsg=error_map[Code.PICERROR])
+
+#
+class Movie(View):
+
+    def get(self, request):
+        return render(request, "index/movie.html")
+
+    def post(self, request):
+        return json.dumps({
+            "errno": '1'
+        })
+
+#
+class MovieDetail(View):
+
+    def get(self, request):
+        return render(request, "index/movieDetail.html")
+
+    def post(self, request):
+        return json.dumps({
+            "errno": '1'
+        })
+
+#
+class Rank(View):
+
+    def get(self, request):
+        return render(request, "index/rank.html")
+
+    def post(self, request):
+        return json.dumps({
+            "errno": '1'
+        })
+
+#
+class Ticket(View):
+
+    def get(self, request):
+        return render(request, "index/ticket.html")
+
+    def post(self, request):
+        return json.dumps({
+            "errno": '1'
+        })
