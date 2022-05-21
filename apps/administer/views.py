61fd1083950bff3fd314048e2f0e0e314a797003
rabbit16 <1214662198@qq.com>
2022-05-22 00:12:30 +0800

    增加了admin后台管理界面

--- apps/administer/views.py
+++ apps/administer/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+
+
+class index(View):
+    def get(self, request):
+        return render(request, 'admin/index.html')
