cee4821f90dc0e22f827cadc172b2e14370e9113
rabbit16 <1214662198@qq.com>
2022-05-30 10:35:29 +0800

    购票

--- static/js/ticket.js
+++ static/js/ticket.js
@@ -55,7 +55,7 @@ const container = document.querySelector(".container");
 				}
 				  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
 				  //意思是option的index值,在不为空的条件下,点击的是哪一个就赋值设置哪一个·的座位与票价。
-				  // movieSelect.selectedIndex 代表哪一个option从0开始哦
+				  // movieSelect.selectedIndex 代表哪一个option从0开始
 				   if (selectedMovieIndex !== null) {
 	    movieSelect.selectedIndex = selectedMovieIndex;
 	  }
